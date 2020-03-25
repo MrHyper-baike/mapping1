a0027c44e6aa6d533e8779cd2a62a6046db3e255
Bakyt Mambetaliev <bakyt.customs@gmail.com>
2020-03-25 19:53:20 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/mappers/ClassMapper.java
@@ -0,0 +1,22 @@
+package kg.megacom.mappers;
+
+import kg.megacom.model.City;
+import kg.megacom.model.House;
+import kg.megacom.model.dto.CityHouse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ClassMapper {
+    ClassMapper INSTANCE= Mappers.getMapper(ClassMapper.class);
+    @Mappings({
+            @Mapping(source = "id", target = "cityId"),
+            @Mapping(source = "id", target = "houseId"),
+            @Mapping(source = "name", target = "cityName"),
+            @Mapping(source = "name", target = "houseName"),
+    })
+    CityHouse cityAndHouseToCytyDto (City city, House house);
+
+}
